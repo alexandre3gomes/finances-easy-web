11bc5365b2ab9472d2f2607cb1b4457afe344ca4
Alexandre Gomes <alexandre3gomes@gmail.com>
2019-03-08 09:21:10 +0000

    Adjusted server.js to heroku deploy

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
 
-var app = express();
-app.use(bodyParser.json());
+const app = express();
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-app.listen(8080);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+console.log('Listen on the port 8080');
