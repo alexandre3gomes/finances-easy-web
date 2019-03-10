a3dd255556b0164d20368484f25ade57bc84e486
Alexandre Gomes <alexandre3gomes@gmail.com>
2019-03-10 23:16:26 +0000

    Added error handler on header.interceptor

--- src/app/shared/interceptors/header.interceptor.ts
+++ src/app/shared/interceptors/header.interceptor.ts
@@ -1,15 +1,25 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
-	constructor() { }
+	constructor(private router: Router) { }
 
 	intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		const token = localStorage.getItem('token');
 		const modified = req.clone({ setHeaders: { 'Authorization': 'Bearer ' + token } });
-		return next.handle(modified);
+		return next.handle(modified).pipe(
+			map((event: HttpEvent<any>) => {
+				return event;
+			}),
+			catchError((error: HttpErrorResponse) => {
+				this.router.navigate([ '/login' ]);
+				return throwError(error);
+			})
+		);
 	}
 }
