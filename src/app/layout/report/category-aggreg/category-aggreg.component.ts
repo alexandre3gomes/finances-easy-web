4189c6c29ef4f5105308b57ef8eb82d61c6aba16
Alexandre Gomes <alexandre3gomes@gmail.com>
2019-05-13 09:10:46 +0100

    Implementing balance chart

--- src/app/layout/report/category-aggreg/category-aggreg.component.ts
+++ src/app/layout/report/category-aggreg/category-aggreg.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Default } from '../../../shared/enum/default.enum';
+import { Budget } from '../../../shared/model/budget/budget.model';
+import { Pagination } from '../../../shared/model/pagination/pagination.model';
+import { ShowAlertError } from '../../../store/alert.actions';
+import { AppState } from '../../../store/app.reducers';
+import { report } from '../../../store/app.selectors';
+import { ListBudgets, ResetBudgets } from '../../budget/store/budget.actions';
+import { budgets } from '../../budget/store/budget.selectors';
+import { ListCategoryAggregValues } from '../store/report.actions';
 
 @Component({
-  selector: 'app-category-aggreg',
-  templateUrl: './category-aggreg.component.html',
-  styleUrls: ['./category-aggreg.component.scss']
+	selector: 'app-category-aggreg',
+	templateUrl: './category-aggreg.component.html',
+	styleUrls: [ './category-aggreg.component.scss' ]
 })
 export class CategoryAggregComponent implements OnInit {
 
-  constructor() { }
+	public state = this.store.select(report);
+	public budgets = this.store.select(budgets);
+	public budget: Budget;
+	public reportForm: FormGroup;
 
-  ngOnInit() {
-  }
+	constructor(public store: Store<AppState>) { }
+
+	ngOnInit () {
+		this.reportForm = new FormGroup({
+			'budget': new FormControl(this.budget, Validators.required),
+		});
+		this.store.dispatch(new ListBudgets(new Pagination(Default.START_PAGE, Default.MAX_SIZE)));
+		this.budgets.subscribe((stateBudgets: Budget[]) => {
+			if (stateBudgets.length === 1) {
+				this.store.dispatch(new ListCategoryAggregValues(stateBudgets[ 0 ].id));
+			}
+		});
+	}
+
+	ngOnDestroy () {
+		this.store.dispatch(new ResetBudgets());
+	}
+
+	loadBudgetData () {
+		if (this.budget) {
+			this.store.dispatch(new ListCategoryAggregValues(this.budget.id));
+		} else {
+			this.store.dispatch(new ShowAlertError('Please select a budget'));
+		}
+	}
 
 }
--- src/app/layout/report/report.component.ts
+++ src/app/layout/report/report.component.ts
@@ -1,51 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
-import { Default } from '../../shared/enum/default.enum';
-import { Budget } from '../../shared/model/budget/budget.model';
-import { Pagination } from '../../shared/model/pagination/pagination.model';
-import { ShowAlertError } from '../../store/alert.actions';
-import { AppState } from '../../store/app.reducers';
-import { report } from '../../store/app.selectors';
-import { ListBudgets, ResetBudgets } from '../budget/store/budget.actions';
-import { budgets } from '../budget/store/budget.selectors';
-import { ListCategoryAggregValues } from './store/report.actions';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-report',
 	templateUrl: './report.component.html',
 	styleUrls: [ './report.component.scss' ]
 })
-export class ReportComponent implements OnInit, OnDestroy {
+export class ReportComponent {
 
-	public state = this.store.select(report);
-	public budgets = this.store.select(budgets);
-	public budget: Budget;
-	public reportForm: FormGroup;
-
-	constructor(public store: Store<AppState>) { }
-
-	ngOnInit () {
-		this.reportForm = new FormGroup({
-			'budget': new FormControl(this.budget, Validators.required),
-		});
-		this.store.dispatch(new ListBudgets(new Pagination(Default.START_PAGE, Default.MAX_SIZE)));
-		this.budgets.subscribe((stateBudgets: Budget[]) => {
-			if (stateBudgets.length === 1) {
-				this.store.dispatch(new ListCategoryAggregValues(stateBudgets[ 0 ].id));
-			}
-		});
-	}
-
-	ngOnDestroy () {
-		this.store.dispatch(new ResetBudgets());
-	}
-
-	loadBudgetData () {
-		if (this.budget) {
-			this.store.dispatch(new ListCategoryAggregValues(this.budget.id));
-		} else {
-			this.store.dispatch(new ShowAlertError('Please select a budget'));
-		}
-	}
 }
