fd561cbc8bc2396166fca6cc7b11af1a2e430842
Alexandre Gomes <alexandre3gomes@gmail.com>
2019-05-23 16:59:54 +0100

    Added balance to dashboard

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-
 import { routerTransition } from '../../router.animations';
 import { BarChart } from '../../shared/model/charts/bar-chart.model';
 import { PieChart } from '../../shared/model/charts/pie-chart.model';
@@ -11,10 +10,11 @@ import { dashboard } from '../../store/app.selectors';
 import { FetchData, ResetData } from './store/dashboard.actions';
 import { DashboardState } from './store/dashboard.reducers';
 
+
 @Component({
 	selector: 'app-dashboard',
 	templateUrl: './dashboard.component.html',
-	animations: [routerTransition()]
+	animations: [ routerTransition() ]
 })
 export class DashboardComponent implements OnInit, OnDestroy {
 
@@ -23,31 +23,33 @@ export class DashboardComponent implements OnInit, OnDestroy {
 	public pieChart = new PieChart();
 	public barChart = new BarChart();
 	public totalIncome = 0;
+	public totalExpense = 0;
 
 	constructor(public store: Store<AppState>, public dateLocale: DateLocaleFilterPipe) { }
 
-	ngOnInit() {
+	ngOnInit () {
 		let x = 0;
 		this.store.dispatch(new FetchData());
 		this.state.subscribe((dashboardState: DashboardState) => {
 			const grouped = dashboardState.expenses.reduce((mapped, exp) => {
-				mapped[exp.category.name] = (mapped[exp.category.name] || 0) + (exp.value);
+				mapped[ exp.category.name ] = (mapped[ exp.category.name ] || 0) + (exp.value);
 				return mapped;
 			}, {});
 			if (this.pieChart.chartData.length == 0) {
 				Object.keys(grouped).forEach(cat => {
 					this.pieChart.addChartLabel(cat);
-					this.pieChart.addChartData(grouped[cat]);
+					this.pieChart.addChartData(grouped[ cat ]);
 				});
 			}
 			this.totalIncome = dashboardState.incomes.reduce((inc, inc1) => inc + inc1.value, 0);
+			this.totalExpense = dashboardState.expenses.reduce((exp, exp1) => exp + exp1.value, 0);
 			let plannedValues = [];
 			let actualValues = [];
 			if (this.barChart.chartLabels.length == 0) {
 				dashboardState.categories.forEach((catVal: CategoryAggregValues) => {
 					this.barChart.addChartLabel(catVal.category.name);
-					plannedValues.push(catVal.periodValue[0].plannedValue);
-					actualValues.push(catVal.periodValue[0].actualValue);
+					plannedValues.push(catVal.periodValue[ 0 ].plannedValue);
+					actualValues.push(catVal.periodValue[ 0 ].actualValue);
 				});
 				if (plannedValues.length > 0 && actualValues.length > 0) {
 					this.barChart.addChartData({ data: plannedValues, label: 'Planned' });
@@ -57,7 +59,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
 		});
 	}
 
-	ngOnDestroy() {
+	ngOnDestroy () {
 		this.store.dispatch(new ResetData());
 	}
 }
