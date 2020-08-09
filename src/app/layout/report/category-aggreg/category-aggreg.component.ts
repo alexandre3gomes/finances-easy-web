f549490873e436010c50e531da6249cab7cea263
Alexandre Gomes <alexandre3gomes@gmail.com>
2020-08-09 13:55:05 +0100

    Changing real x planned report

--- src/app/layout/report/category-aggreg/category-aggreg.component.ts
+++ src/app/layout/report/category-aggreg/category-aggreg.component.ts
@@ -5,15 +5,16 @@ import { Store } from '@ngrx/store';
 import { Default } from '../../../shared/enum/default.enum';
 import { Budget } from '../../../shared/model/budget/budget.model';
 import { Pagination } from '../../../shared/model/pagination/pagination.model';
+import { CategoryAggregValues } from '../../../shared/model/report/category-aggreg-values.model';
 import { ShowAlertError } from '../../../store/alert.actions';
 import { AppState } from '../../../store/app.reducers';
 import { report } from '../../../store/app.selectors';
 import { ListBudgets, ResetBudgets } from '../../budget/store/budget.actions';
 import { budgets } from '../../budget/store/budget.selectors';
-import { ListCategoryAggregValues } from '../store/report.actions';
+import { ListCategoryAggregValues, ListIncomePeriod } from '../store/report.actions';
 
 @Component({
-	selector: 'app-category-aggreg',
+	selector: 'app-category-aggreg-component',
 	templateUrl: './category-aggreg.component.html',
 	styleUrls: [ './category-aggreg.component.scss' ]
 })
@@ -21,8 +22,13 @@ export class CategoryAggregComponent implements OnInit, OnDestroy {
 
 	public state = this.store.select(report);
 	public budgets = this.store.select(budgets);
-	public budgetId: number = -1;
 	public reportForm: FormGroup;
+	public periods: Array<Date>;
+	public catAggValues: Array<CategoryAggregValues>;
+	public total: Array<number>;
+	public balance: Array<number>;
+	public FORECAST_INCOME = 2591;
+	public budgetId = -1;
 
 	constructor(public store: Store<AppState>) { }
 
@@ -32,10 +38,33 @@ export class CategoryAggregComponent implements OnInit, OnDestroy {
 		});
 		this.store.dispatch(new ListBudgets(new Pagination(Default.START_PAGE, Default.MAX_SIZE)));
 		this.budgets.subscribe((stateBudgets: Budget[]) => {
-			let currBudget = stateBudgets.filter(bud => new Date(bud.startDate) <= new Date() && new Date(bud.endDate) >= new Date());
+			const currBudget = stateBudgets.filter(bud => new Date(bud.startDate) <= new Date() && new Date(bud.endDate) >= new Date());
 			if (currBudget.length > 0) {
 				this.budgetId = currBudget[0].id;
 				this.store.dispatch(new ListCategoryAggregValues(currBudget[0].id));
+				this.store.dispatch(new ListIncomePeriod(currBudget[0].id));
+			}
+		});
+		this.state.subscribe((repState) => {
+			this.total = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+			this.balance = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+			repState.catVal.forEach(catVal => {
+				this.periods = catVal.periodValue.map(per => {
+					return per.startDate;
+				});
+				for (let x = 0; x <= catVal.periodValue.length; x++) {
+					if (catVal.periodValue[ x ]) {
+						if (new Date(catVal.periodValue[ x ].endDate).getTime() <= new Date().getTime()) {
+							this.total[ x ] += catVal.periodValue[ x ].actualValue;
+						} else {
+							this.total[ x ] += catVal.periodValue[ x ].plannedValue;
+						}
+					}
+				}
+			});
+			for (let x = 0; x < this.total.length; x++) {
+				const income = (repState.incomePeriod[ x ] > 0 ? repState.incomePeriod[ x ] : this.FORECAST_INCOME);
+				this.balance[x] = income - this.total[x];
 			}
 		});
 	}
@@ -46,10 +75,13 @@ export class CategoryAggregComponent implements OnInit, OnDestroy {
 
 	loadBudgetData() {
 		if (+this.reportForm.get('budget').value > 0) {
-			this.store.dispatch(new ListCategoryAggregValues(+this.reportForm.get('budget').value ));
+			this.store.dispatch(new ListCategoryAggregValues(+this.reportForm.get('budget').value));
 		} else {
 			this.store.dispatch(new ShowAlertError('Please select a budget'));
 		}
 	}
 
+	isActual(date: Date) {
+		return new Date(date).getTime() <= new Date().getTime();
+	}
 }
