6d4d8ad0205e3a90f3b2d21f8587596eb59fdb9b
Alexandre Silva <alexandre@youcanbook.me>
2022-08-11 08:12:04 +0100

    Adding build.yaml to deploy to AWS Beanstalk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    api: 'https://api.finances-easy.com/',
+    api: 'https://api.finances-easy.co.uk/',
     okta: {
         clientId: '0oa4e4x01zItFJ2dF5d6',
         issuer: 'https://dev-2225315.okta.com/oauth2/default',
-        redirectUri: 'https://finances-easy.com/callback',
+        redirectUri: 'https://finances-easy.co.uk/callback',
         scopes: ['openid', 'profile', 'email'],
         pkce: true
     }
